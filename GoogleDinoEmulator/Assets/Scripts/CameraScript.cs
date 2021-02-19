234d3469a37aaf99eac46fef0b62a5f0126d9e87
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-02-19 21:35:34 +0000

    Updating to Unity 2019.3.13f1, removing ground  objects after 5 seconds

--- GoogleDinoEmulator/Assets/Scripts/CameraScript.cs
+++ GoogleDinoEmulator/Assets/Scripts/CameraScript.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour {
-
+public class CameraScript : MonoBehaviour
+{
     public GameObject player;
-	
-	void Update () {                                        // x has an offset so the player is displayed to the left -> OG: 11, 1
-        this.gameObject.transform.position = new Vector3((player.gameObject.transform.position.x + 6 ), 2, this.gameObject.transform.position.z);	
-	} 
+
+    private void Update()
+    {
+        this.gameObject.transform.position = new Vector3((player.gameObject.transform.position.x + 3), 2, this.gameObject.transform.position.z);
+    }
 }
