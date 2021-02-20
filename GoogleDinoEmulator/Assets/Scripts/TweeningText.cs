895355481cb0bf5470e4a9706ebddaac1431a278
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-02-20 13:04:13 +0000

    Adding Title Screen + Transition Screen to Game (UI)

---
+++ GoogleDinoEmulator/Assets/Scripts/TweeningText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TweeningText : MonoBehaviour
+{
+    public int maxSize;
+    public int minSize;
+    public int increment;
+    public Text m_Text;
+
+    private bool flag = true;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (flag)
+        {
+            if (m_Text.fontSize <= maxSize)
+                m_Text.fontSize += increment;
+            else
+                flag = false;
+        }
+        else
+        {
+            if (m_Text.fontSize <= minSize)
+                flag = true;
+            else
+                m_Text.fontSize -= increment;
+        }
+    }
+}
