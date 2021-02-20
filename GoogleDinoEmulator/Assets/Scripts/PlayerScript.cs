46688b1b2d742291ae822fe6c3fec51fbc17948b
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-02-20 12:06:30 +0000

    Fixing speed ramp-up, adding UI (Score)

--- GoogleDinoEmulator/Assets/Scripts/PlayerScript.cs
+++ GoogleDinoEmulator/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -10,24 +11,24 @@ public class PlayerScript : MonoBehaviour
     public float playerSpeed;
     public Transform groundCheck;
 
+    public Text scoreText;
+
     private bool isGround = false;
     private Vector3 playerVelocity;
 
     private void Start()
     {
         playerVelocity = Vector2.zero;
-        playerSpeed = baseSpeed;
+        playerSpeed = 10;
+        StartCoroutine(rampUpSpeed());
     }
 
     private void Update()
     {
         isGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-        playerScore = transform.position.x / 4;
+        playerScore = transform.position.x / 2;
 
-        if (playerScore >= 100f)
-            playerSpeed = 40;
-        else if (playerScore > 12)
-            playerSpeed = playerScore;
+        StartCoroutine(updateUI());
 
         playerVelocity.x = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
 
@@ -54,6 +55,24 @@ public class PlayerScript : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Time.timeScale = 0;
+        }
+    }
+
+    private IEnumerator updateUI()
+    {
+        yield return new WaitForEndOfFrame();
+        scoreText.text = $"Score: {playerScore.ToString("0")}";
+        yield return new WaitForEndOfFrame();
+    }
+
+    private IEnumerator rampUpSpeed()
+    {
+        yield return new WaitForSeconds(2);
+
+        if (playerSpeed < 20)
+        {
+            playerSpeed += 2;
+            StartCoroutine(rampUpSpeed());
         }
     }
 }
